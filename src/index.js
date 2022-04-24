be0be069000009f859a62ee026edc0f71cffaaa1
Nader Elsarrag <elsarragnader@gmail.com>
2022-04-24 17:15:43 -0500

    initial redux setup, fixed a bug where an misnamed variable broke the entire website...

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
