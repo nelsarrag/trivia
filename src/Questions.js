d4f3f45b453beecae3cb671dc528be4d07ace4a7
Nader Elsarrag <elsarragnader@gmail.com>
2022-04-24 01:48:14 -0500

    setup for questions page including scaffold w/ mui components and testing access w/ API url

--- src/Questions.js
+++ src/Questions.js
@@ -1,9 +1,27 @@
+import { Box, Button, Typography } from "@mui/material";
+import useAxios from "./useAxios";
 
 const Questions = () => {
+
+    let apiUrl = `/api.php?amount=10`;
+
+    const { response, loading } = useAxios({ url: apiUrl })
+    console.log(response);
+
     return (
-        <div>
-            Questions
-        </div>
+        <Box>
+            <Typography variant="h4">Question 1</Typography>
+            <Typography mt={5}>This is the question</Typography>
+            <Box mt={2}>
+                <Button variant="contained">Answer 1</Button>
+            </Box> 
+            <Box mt={2}>
+                <Button variant="contained">Answer 2</Button>
+            </Box>
+            <Box mt={5}>
+                Score: 2 / 5
+            </Box>
+        </Box>
     )
 }
 
