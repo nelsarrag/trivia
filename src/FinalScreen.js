54f9fc43414a78f37d9d94af10e93a12d851f1d1
Nader Elsarrag <elsarragnader@gmail.com>
2022-04-24 19:28:39 -0500

    finalized final screen page to show final score and includes a button to return to settings (play again)

--- src/FinalScreen.js
+++ src/FinalScreen.js
@@ -1,9 +1,28 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleAmountChange, handleScoreChange } from "./actions";
 
 const FinalScreen = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { score } = useSelector(state => state);
+
+    const handleBackToSettings = () => {
+        dispatch(handleScoreChange(0));
+        dispatch(handleAmountChange(50));
+        navigate("/");
+    }
     return (
-        <div>
-            Final Score
-        </div>
+        <Box mt={30}>
+            <Typography variant="h3" fontWeight="bold" mb={3}>
+                Final Score: {score}
+            </Typography>
+            <Button onClick={handleBackToSettings} variant="outlined">
+                Play again!
+            </Button>
+        </Box>
     )
 }
 
