e7a0f01e1b004b7d238d09675352987873a49757
Nader Elsarrag <elsarragnader@gmail.com>
2022-04-24 19:19:07 -0500

    finalized redux, finalized questions page, returns visual feedback when user selects a correct answer with a running score count

--- src/Questions.js
+++ src/Questions.js
@@ -1,6 +1,9 @@
 import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import { decode } from "html-entities";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleScoreChange } from "./actions";
 import useAxios from "./useAxios";
 
 const getRandomInt = (max) => {
@@ -13,7 +16,11 @@ const Questions = () => {
         question_difficulty,
         question_type,
         amount_of_question,
+        score,
     } = useSelector((state) => state);
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     let apiUrl = `/api.php?amount=${amount_of_question}`;
     if(question_category) {
@@ -31,7 +38,6 @@ const Questions = () => {
     const { response, loading } = useAxios({ url: apiUrl })
     const [questionIndex, setQuestionIndex] = useState(0);
     const [options, setOptions] = useState([]);
-    console.log(options);
     useEffect(() => {
         if(response?.results.length) {
             const question = response.results[questionIndex];
@@ -43,7 +49,7 @@ const Questions = () => {
             );
             setOptions(answers);
         }
-    }, [response]);
+    }, [response, questionIndex]);
 
     if(loading) {
         return (
@@ -53,17 +59,32 @@ const Questions = () => {
         )
     }
 
+    const handleClickAnswer = (e) => {
+        const question = response.results[questionIndex];
+
+        if(e.target.textContent === question.correct_answer) {
+            dispatch(handleScoreChange(score + 1));
+        }
+        if(questionIndex + 1 < response.results.length) {
+            setQuestionIndex(questionIndex + 1);
+        } else {
+            navigate('/score');
+        }
+    };
+
     return (
         <Box>
             <Typography variant="h4">Question {questionIndex + 1}</Typography>
-            <Typography mt={5}>{response.results[questionIndex].question}</Typography>
+            <Typography mt={5}>{decode(response.results[questionIndex].question)}</Typography>
             {options.map((data, id) => (
                 <Box mt={2} key={id}>
-                    <Button variant="contained">{data}</Button>
+                    <Button onClick={handleClickAnswer} variant="contained">
+                        {decode(data)}
+                    </Button>
                 </Box>
             ))}
             <Box mt={5}>
-                Score: 2 / 5
+                Score: {score} / {response.results.length}
             </Box>
         </Box>
     )
